e08bb98349e92bd33e90e43a1306bf6ce01ec042
Malte <malte.sohns@googlemail.com>
2019-04-07 14:38:34 +0200

    small fix

--- pwned.py
+++ pwned.py
@@ -1,37 +1,40 @@
-import hashlib, requests, json, sys, argparse
+import hashlib
+import requests
+import json
+import argparse
+import sys
+
 
 def check_pwned_pwd(pw_in):    
     for pw in pw_in:
         try:
             sh1hash = hashlib.sha1(pw.encode('utf-8')).hexdigest().upper()
             head, tail = sh1hash[:5], sh1hash[5:]
-            r = requests.get('https://api.pwnedpasswords.com/range/{0}'.format(head))
-            
+            r = requests.get(f'https://api.pwnedpasswords.com/range/{head}')
             hashes = (line.split(':') for line in r.text.splitlines())
-            count = next((int(count) for t, count in hashes if t == tail),0)
+            count = next((int(count) for t, count in hashes if t == tail), 0)
             if count:
-                print('Password : "{0}" found {1} times [{2}]'.format(pw, count, sh1hash))
+                print(f'Password : "{pw}" found {count} times [{sh1hash}]')
             else:
                 print('Your password has not been pwned...yet! Keep Safe')
         except Exception:
-            print('Something went wrong : {0}'.format(sys.exc_info()[0]))
+            print(f'Something went wrong : {sys.exc_info()[0]}')
+
 
 def check_pwned_account(accounts):
     for account in accounts:
-        r = requests.get('https://haveibeenpwned.com/api/v2/breachedaccount/{0}'.format(account))
+        r = requests.get(f'https://haveibeenpwned.com/api/v2/breachedaccount/{account}')
         if r.text:
             for breach in r.json():
-                print('{0} -> Account breached at : {1} - {2}'.format(account,breach['Name'], breach['BreachDate']))
+                print(f'{account} -> Account breached at : {breach["Name"]} - {breach["BreachDate"]}')
         else:
             print("No breach as been found")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Check if a user account or password has been found in the haveibeenpwned database")
-    parser.add_argument('-p','--p', nargs='+')
-    parser.add_argument('-u','--u', nargs='+')
-    
+    parser.add_argument('-p', '--p', nargs='+')
+    parser.add_argument('-u', '--u', nargs='+')
     args = parser.parse_args()
-
     if args.p:
         check_pwned_pwd(args.p)
     if args.u:
