bea870f88be710c21cc2c9c37748093d2f869fee
rangulvers <malte.sohns@googlemail.com>
2022-02-17 14:37:23 +0100

    renamed beenpwned

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="py-h1bp",
+    name="beenpwned",
     version="0.1.0",
     author="rangulvers",
     description="Check if you have been pwned",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/rangulvers/h1bp",
+    url="https://github.com/rangulvers/beenpwned",
     project_urls={
-        "Bug Tracker": "https://github.com/rangulvers/h1bp/issues",
+        "Bug Tracker": "https://github.com/rangulvers/beenpwned/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
